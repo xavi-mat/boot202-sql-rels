1bd4a6d5ac57727e60fe076bde161c9e46ff4399
Xavimat <xaviermatoses@gmail.com>
2022-05-14 22:58:00 +0200

    Exercise 2.2.3. Get data

--- 04_get_data.sql
+++ 04_get_data.sql
@@ -14,15 +14,44 @@ SELECT * FROM products WHERE price > 20;
 SELECT * FROM products ORDER BY price DESC;
 
 -- Seleccione todos los productos y que muestre la categoría a la que pertenecen.
-SELECT p.id, p.title, p.price, c.name
+SELECT p.id, p.author, p.title, p.price, c.name AS category
     FROM products p
     JOIN categories c
     ON p.category_id = c.id;
 
 -- Seleccione todos los usuarios y muestre sus pedidos.
-SELECT u.id,
-    FROM 
-
+-- Versión con el total, sin el detalle del pedido
+SELECT u.id, u.name, o.id AS order_id, o.date, o.status, SUM(d.quantity) AS num_items, ROUND(SUM(d.price*d.quantity), 2) AS total
+    FROM users u
+    JOIN orders o ON o.user_id = u.id
+    JOIN details d ON d.order_id=o.id
+    GROUP BY u.id;
+-- Versión con el detalle del pedido
+SELECT u.id, u.name, o.id AS order_id, o.date, o.status, d.product_id, p.author, p.title, d.quantity, p.price, ROUND((d.quantity * p.price), 2) AS subtotal
+    FROM users u
+    JOIN orders o ON o.user_id = u.id
+    JOIN details d ON d.order_id=o.id
+    JOIN products p ON d.product_id=p.id
+    ORDER BY u.id, o.id;
 
 -- Selecciona un producto por su id y que muestre la categoría a la que pertenece.
+SELECT p.author, p.title, c.name AS category
+	FROM products p
+    JOIN categories c ON p.category_id=c.id
+    WHERE p.id = 3;
+
 -- Seleccione a un usuario por su id y muestre los pedidos que tiene.
+-- Versión con el total, sin el detalle del pedido
+SELECT u.id, u.name, o.id AS order_id, o.date, o.status, SUM(d.quantity) AS num_items, ROUND(SUM(d.price*d.quantity), 2) AS total
+    FROM users u
+    JOIN orders o ON o.user_id = u.id
+    JOIN details d ON d.order_id=o.id
+    WHERE u.id=3;
+-- Versión con el detalle del pedido
+SELECT u.id, u.name, o.id AS order_id, o.date, o.status, d.product_id, p.author, p.title, d.quantity, p.price, ROUND((d.quantity * p.price), 2) AS subtotal
+    FROM users u
+    JOIN orders o ON o.user_id = u.id
+    JOIN details d ON d.order_id=o.id
+    JOIN products p ON d.product_id=p.id
+    WHERE u.id = 3
+    ORDER BY o.id;
